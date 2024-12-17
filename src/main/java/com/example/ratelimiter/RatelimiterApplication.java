2fb8a086d97a902c6316158664b72b8ee2daa266
Suyash Jain <suyash.jain@oracle.com>
2024-12-17 15:17:24 +0530

    wip

--- src/main/java/com/example/ratelimiter/RatelimiterApplication.java
+++ src/main/java/com/example/ratelimiter/RatelimiterApplication.java
@@ -3,8 +3,6 @@ package com.example.ratelimiter;
 //import com.example.ratelimiter.filter.RateLimiterFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -16,13 +14,7 @@ public class RatelimiterApplication {
 
 	}
 
-//	@Bean
-//	public FilterRegistrationBean<RateLimiterFilter> rateLimitingFilter() {
-//		FilterRegistrationBean<RateLimiterFilter> registrationBean = new FilterRegistrationBean<>();
-//		registrationBean.setFilter(new RateLimiterFilter());
-//		registrationBean.addUrlPatterns("*"); // Register filter for API endpoints
-//		return registrationBean;
-//	}
+
 
 
 
